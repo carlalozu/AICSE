6bbfc19bac23dc460e92bd1c92091d8d764d7dc0
Carla Lopez <calopez@student.ethz.ch>
2024-04-08 00:54:51 +0200

    add phases

--- project01/Proj1_Y24/Task2/pinn.py
+++ project01/Proj1_Y24/Task2/pinn.py
@@ -6,27 +6,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class Pinns:
     """Class to solve an inverse problem using physics-informed neural network (PINN)"""
 
     def __init__(self, n_int_, n_sb_, n_tb_, t0, tf, alpha_f, h_f,
-                 T_hot, u_f, T0, T_cold):
+                 T_hot, T0, T_cold):
         self.n_int = n_int_
         self.n_sb = n_sb_
         self.n_tb = n_tb_
-
 
         # Parameters of the reaction-convection-diffusion equation
         self.alpha_f = alpha_f
         self.h_f = h_f
         self.T_hot = T_hot
-        self.u_f = u_f
         self.T0 = T0
         self.T_cold = T_cold
 
-        # Extrema of the solution domain (t,x) in [0,0.1] x [-1,1]
-        self.domain_extrema = torch.tensor([[0, 1],  # Time dimension
+        self.t0 = t0
+        self.tf = tf
+
+        # Extrema of the solution domain (t,x)
+        self.domain_extrema = torch.tensor([[t0, tf],  # Time dimension
                                             [0, 1]])  # Space dimension
+
+        # Number of cycles
+        self.n_cycles = self.domain_extrema[0, 1] - self.domain_extrema[0, 0]
+        self.n_sb_cycles = self.n_sb // self.n_cycles
 
         # Number of space dimensions
         self.space_dimensions = 1
@@ -35,7 +41,7 @@ class Pinns:
         self.lambda_u = 10
 
         ########################################################
-        # Create FF Dense NNs for approxiamte solution and approximate coefficient
+        # Create FF Dense NNs for approximate solution and approximate coefficient
 
         # FF Dense NN to approximate the solution of the underlying heat equation
         self.approximate_solution = NeuralNet(
@@ -46,7 +52,7 @@ class Pinns:
             retrain_seed=42
         )
 
-        # FF Dense NN to approximate the conductivity we wish to infer
+        # FF Dense NN to approximate the solid temperature we wish to infer
         self.approximate_coefficient = NeuralNet(
             input_dimension=self.domain_extrema.shape[0],
             output_dimension=1,
@@ -56,15 +62,17 @@ class Pinns:
         )
 
         ########################################################
-        max_iter = 10000
-        self.optimizer = torch.optim.LBFGS(list(self.approximate_solution.parameters()) + list(self.approximate_coefficient.parameters()),
-                              lr=float(0.5),
-                              max_iter=max_iter,
-                              max_eval=50000,
-                              history_size=150,
-                              line_search_fn="strong_wolfe",
-                              tolerance_change=1.0 * np.finfo(float).eps)
-
+        max_iter = 5000
+        self.optimizer = torch.optim.LBFGS(
+            list(self.approximate_solution.parameters()) +
+            list(self.approximate_coefficient.parameters()),
+            lr=float(0.5),
+            max_iter=max_iter,
+            max_eval=50000,
+            history_size=150,
+            line_search_fn="strong_wolfe",
+            tolerance_change=1.0 * np.finfo(float).eps
+        )
 
         # Generator of Sobol sequences --> Sobol sequences (see
         # https://en.wikipedia.org/wiki/Sobol_sequence)
@@ -74,10 +82,7 @@ class Pinns:
         # Training sets S_sb, S_tb, S_int as torch dataloader
         self.training_set_sb, self.training_set_tb, self.training_set_int = self.assemble_datasets()
 
-        # number of sensors to record temperature
-        self.n_sensor = 50
 
-    #######################################################################
     def convert(self, tens):
         """Function to linearly transform a tensor whose value are between 0 and
         1 to a tensor whose values are between the domain extrema"""
@@ -85,8 +90,6 @@ class Pinns:
         return tens * (self.domain_extrema[:, 1] - self.domain_extrema[:, 0]
                        ) + self.domain_extrema[:, 0]
 
-    #######################################################################
-    
     def initial_condition(self, x):
         """Initial condition to solve the equation, T0"""
         return torch.ones_like(x)*self.T0
@@ -95,12 +98,34 @@ class Pinns:
         """Function returning the input-output tensor required to assemble the
         training set S_tb corresponding to the temporal boundary """
 
-        t0 = self.domain_extrema[0, 0]
         input_tb = self.convert(self.soboleng.draw(self.n_tb))
-        input_tb[:, 0] = torch.full(input_tb[:, 0].shape, t0)
+        input_tb[:, 0] = torch.full(input_tb[:, 0].shape, self.t0)
         output_tb = self.initial_condition(input_tb[:, 1]).reshape(-1, 1)
 
         return input_tb, output_tb
+
+    def add_spatial_boundary_points_charging(self, x):
+        """Boundary points for charging phase"""
+        # Dirichlet boundary condition
+        output_sb_0 = torch.zeros((x.shape[0], 1)) + self.T_hot
+        # Zero Neumann boundary condition
+        output_sb_L = torch.zeros((x.shape[0], 1))
+        return output_sb_0, output_sb_L
+
+    def add_spatial_boundary_points_discharging(self, x):
+        """Boundary points for discharging phase"""
+        # Dirichlet boundary condition
+        output_sb_L = torch.zeros((x.shape[0], 1)) + self.T_cold
+        # Zero Neumann boundary condition
+        output_sb_0 = torch.zeros((x.shape[0], 1))
+        return output_sb_0, output_sb_L
+
+    def add_spatial_boundary_points_idle(self, x):
+        """Boundary points for idle phase"""
+        # Zero Neumann boundary condition
+        output_sb_L = torch.zeros((x.shape[0], 1))
+        output_sb_0 = torch.zeros((x.shape[0], 1))
+        return output_sb_0, output_sb_L
 
     def add_spatial_boundary_points(self):
         """Function returning the input-output tensor required to assemble the
@@ -109,6 +134,7 @@ class Pinns:
         x0 = self.domain_extrema[1, 0]
         xL = self.domain_extrema[1, 1]
 
+        # Add input coordinates
         input_sb = self.convert(self.soboleng.draw(self.n_sb))
 
         input_sb_0 = torch.clone(input_sb)
@@ -117,11 +143,20 @@ class Pinns:
         input_sb_L = torch.clone(input_sb)
         input_sb_L[:, 1] = torch.full(input_sb_L[:, 1].shape, xL)
 
-        # Dirichlet boundary condition
-        output_sb_0 = torch.zeros((input_sb.shape[0], 1)) + self.T_hot
-
-        # Zero Neumann boundary condition
-        output_sb_L = torch.zeros((input_sb.shape[0], 1))
+        # Add output coordinates
+        for i in range(self.t0, self.tf):
+            if i % 4 == 0:      # Multiple of 4
+                print("Charging")
+                output_sb_0, output_sb_L = self.add_spatial_boundary_points_charging(
+                    input_sb_0)
+            elif i % 2 != 0:    # Odd number
+                print("Idle")
+                output_sb_0, output_sb_L = self.add_spatial_boundary_points_idle(
+                    input_sb_0)
+            else:               # Else
+                print("Discharging")
+                output_sb_0, output_sb_L = self.add_spatial_boundary_points_discharging(
+                    input_sb_0)
 
         return torch.cat([input_sb_0, input_sb_L], 0), torch.cat([output_sb_0, output_sb_L], 0)
 
@@ -129,15 +164,10 @@ class Pinns:
         """Function returning the input-output tensor required to assemble the
         training set S_int corresponding to the interior domain where the PDE is
         enforced"""
-
-        ########################################################
         # Return input-output tensor required to assemble the training set S_int
         # corresponding to the interior domain where the PDE is enforced
-
         input_int = self.convert(self.soboleng.draw(self.n_int))
         output_int = torch.zeros((input_int.shape[0], 1))
-
-        ########################################################
 
         return input_int, output_int
 
@@ -146,9 +176,7 @@ class Pinns:
         # TODO: Think about logic on measurements
         """
         data = pd.read_csv('DataSolution.txt', sep=",", header=0)
-        t0 = self.domain_extrema[0, 0].item()
-        tf = self.domain_extrema[0, 1].item()
-        data_filtered = data[(data['t'] < tf) & (data['t'] >= t0)]
+        data_filtered = data[(data['t'] < self.tf) & (data['t'] >= self.t0)]
 
         # Extract the data
         x = torch.tensor(data_filtered['x'].unique(), dtype=torch.float)
@@ -180,7 +208,6 @@ class Pinns:
 
         return training_set_sb, training_set_tb, training_set_int
 
-    #######################################################################
     def apply_initial_condition(self, input_tb):
         """Function to compute the terms required in the definition of the
         TEMPORAL boundary residual"""
@@ -193,17 +220,38 @@ class Pinns:
         input_sb.requires_grad = True
         u_pred_sb = self.approximate_solution(input_sb)
 
-        u_x0 = u_pred_sb[:self.n_sb]
-        u_xL = u_pred_sb[self.n_sb:]
+        bound_x0 = None
+        bound_xL = None
+        for i in range(self.t0, self.tf):
+            u_x0 = u_pred_sb[:self.n_sb]
+            u_xL = u_pred_sb[self.n_sb:]
 
+            # Differentiate u_x0 and u_xL with input_sb
+            u_x0_x = torch.autograd.grad(
+                u_x0.sum(), input_sb, create_graph=True)[0][:, 1]    
+            u_x0_x = u_x0_x.reshape(-1, 1)[:self.n_sb]
 
-        # Differentiate u_xL with input_sb
-        u_xL_x = torch.autograd.grad(u_xL.sum(), input_sb,
-                                     create_graph=True)[0][self.n_sb:, 1]
-        u_xL_x = u_xL_x.reshape(-1, 1)
+            u_xL_x = torch.autograd.grad(
+                u_xL.sum(), input_sb, create_graph=True)[0][:, 1]
+            u_xL_x = u_xL_x.reshape(-1, 1)[self.n_sb:]
+
+            if i % 4 == 0: # Charging
+                print("BC for charging phase")
+                bound_x0 = u_x0
+                bound_xL = u_xL_x
+
+            elif i % 2 != 0: # Idle
+                print("BC for idle phase")
+                bound_x0 = u_x0_x
+                bound_xL = u_xL_x
+
+            else: # Discharging
+                print("BC for discharging phase")
+                bound_x0 = u_x0_x
+                bound_xL = u_xL
+
         # Concat
-        u_bound = torch.cat([u_x0, u_xL_x], 0)
-
+        u_bound = torch.cat([bound_x0, bound_xL], 0)
         return u_bound
 
     def compute_pde_residual(self, input_int):
@@ -232,12 +280,17 @@ class Pinns:
         # Compute the second derivative (HINT: Pay attention to the
         # dimensions! --> torch.autograd.grad(..., ..., ...)[...][...]
         ##############
-        grad_u_xx = torch.autograd.grad(grad_u_x.sum(), input_int, create_graph=True)[0][:, 1]
+        grad_u_xx = torch.autograd.grad(
+            grad_u_x.sum(), input_int, create_graph=True)[0][:, 1]
 
+        u_f = 0
+        if self.t0 % 4 == 0:
+            u_f = 1
+        elif self.t0 % 2 != 0:
+            u_f = -1
 
-        ##############
-
-        residual = grad_u_t + self.u_f*grad_u_x - self.alpha_f*grad_u_xx + self.h_f*(u - k)
+        residual = grad_u_t + u_f*grad_u_x - \
+            self.alpha_f*grad_u_xx + self.h_f*(u - k)
 
         return residual.reshape(-1, )
 
@@ -340,7 +393,7 @@ class Pinns:
         output = torch.cat([output_tf, output_ts], 1)
         labels = ["T_f", "T_s"]
         _, axs = plt.subplots(1, 2, figsize=(16, 6), dpi=100)
-        lims = [(1, 4), (0,3)]
+        lims = [(1, 4), (1, 3)]
         for i in range(2):
             im = axs[i].scatter(
                 inputs[:, 0].detach(),
