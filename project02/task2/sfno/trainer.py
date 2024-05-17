c443d6ae1ce32e0fa5702c58a56b8d64389e2ccf
Carla Lopez <calopez@student.ethz.ch>
2024-05-17 14:04:15 +0200

    trainer for snfo

--- project02/task2/sfno/trainer.py
+++ project02/task2/sfno/trainer.py
@@ -1,80 +1,78 @@
 """Trainer for the FNO1d model."""
 import torch
 import numpy as np
-from torch.utils.data import DataLoader, TensorDataset
 from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
 import matplotlib.pyplot as plt
-
+from neuralop.datasets import load_spherical_swe
 from fno02d import FNO2d
+from utils import LpLoss
 
 torch.manual_seed(0)
 np.random.seed(0)
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 
 class Trainer():
-    """Trainer for the FNO1d model."""
+    """Trainer for the FNO3d model."""
 
-    def __init__(self, n_train, modes=16, width=64, batch_size=10):
+    def __init__(self, n_train, modes=32, width=64):
         self.n_train = n_train
-        self.training_set, self.testing_set = self.assemble_datasets(
-            batch_size)
+        self.training_set, self.testing_set = self.assemble_datasets()
 
-        self.fno = FNO2d(modes, width)  # model
+        self.fno = FNO2d(modes, modes, width)  # model
+        # self.fno = FNO3d(n_modes=(32, 32), in_channels=3, out_channels=3,
+        # hidden_channels=32, projection_channels=64, factorization='dense')
+        self.fno = self.fno.to(device)
 
-    def assemble_datasets(self, batch_size=10):
+    def assemble_datasets(self):
         """Load the data and prepare the datasets."""
 
-        # Load the data
-        # - AC_data_input.npy
-        # - AC_data_output.npy
-        x_data = torch.from_numpy(
-            np.load("AC_data_input.npy")).type(torch.float32)
-        y_data = torch.from_numpy(
-            np.load("AC_data_output.npy")).type(torch.float32)
-
-        temporary_tensor = torch.clone(x_data[:, :, 0])
-        x_data[:, :, 0] = x_data[:, :, 1]
-        x_data[:, :, 1] = temporary_tensor
-
-        self.input_function_train = x_data[:self.n_train, :]
-        self.output_function_train = y_data[:self.n_train, :]
-        self.input_function_test = x_data[self.n_train:, :]
-        self.output_function_test = y_data[self.n_train:, :]
-
-        training_set = DataLoader(TensorDataset(
-            self.input_function_train, self.output_function_train),
-            batch_size=batch_size, shuffle=True)
-        testing_set = DataLoader(TensorDataset(
-            self.input_function_test, self.output_function_test),
-            batch_size=batch_size, shuffle=False)
-
+        training_set, testing_set = load_spherical_swe(
+            n_train=200,
+            batch_size=4,
+            train_resolution=(32, 64),
+            test_resolutions=[(32, 64), (64, 128)],
+            n_tests=[50, 50],
+            test_batch_sizes=[10, 10],
+        )
         return training_set, testing_set
 
-    def plot_inputs(self, id_sample=32):
+    def plot_inputs(self):
         """Plot the input and output functions."""
-        plt.figure()
-        plt.plot(
-            self.input_function_train[id_sample, :, 1],
-            self.input_function_train[id_sample, :, 0],
-            label="input:  $t = 0$")
-        plt.plot(
-            self.input_function_train[id_sample, :, 1],
-            self.output_function_train[id_sample, :],
-            label="output: $t = 1$")
-        plt.grid(True, which="both", ls=":")
-        plt.xlabel("x")
-        plt.ylabel("u(x, t)")
-        plt.legend()
+        fig, ax = plt.subplots(1, 2, figsize=(7, 3))
+        fig.suptitle('Inputs and ground-truth output')
 
-    def train(self, epochs, learning_rate, step_size, gamma):
+        test_samples = self.training_set.dataset
+        data = test_samples[0]
+
+        # Input x
+        x = data['x'][0, ...].detach().numpy()
+        # Ground-truth
+        y = data['y'][0, ...].numpy()
+
+        ax[0].imshow(x)
+        ax[0].set_title('Input x')
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+
+        ax[1].imshow(y)
+        ax[1].set_title('Ground-truth y')
+        ax[1].set_xticks([])
+        ax[1].set_yticks([])
+
+        plt.tight_layout()
+        plt.show()
+
+    def train(self, epochs, learning_rate=8e-4):
         """Train the model."""
 
         optimizer = Adam(self.fno.parameters(),
                          lr=learning_rate, weight_decay=1e-5)
-        scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer, step_size=step_size, gamma=gamma)
+        scheduler = CosineAnnealingLR(optimizer, T_max=30)
 
-        loss = torch.nn.MSELoss()
+        loss = LpLoss(d=2, p=2, reduce_dims=(0, 1))
         freq_print = 1
         for epoch in range(epochs):
             train_mse = 0.0
@@ -109,31 +107,39 @@ class Trainer():
         ).reshape(-1, )) ** p) / torch.mean(abs(y.detach()) ** p)) ** (1 / p) * 100
         return err
 
-    def plot(self, idx_data=134):
+    def plot(self):
         """Plot results"""
-        input_function_test_n = self.input_function_test[idx_data, :].unsqueeze(
-            0)
-        output_function_test_n = self.output_function_test[idx_data, :].unsqueeze(
-            0)
-        # print(input_function_test_n.shape)
-        # print(output_function_test_n.shape)
+        fig = plt.figure(figsize=(7, 7))
+        for index, resolution in enumerate([(32, 64), (64, 128)]):
+            test_samples = self.testing_set[resolution].dataset
+            data = test_samples[0]
+            # Input x
+            x = data['x']
+            # Ground-truth
+            y = data['y'][0, ...].numpy()
+            # Model prediction
+            x_in = x.unsqueeze(0).to(device)
+            out = self.fno(x_in).squeeze()[0, ...].detach().cpu().numpy()
+            x = x[0, ...].detach().numpy()
 
-        output_function_test_pred_n = self.fno(input_function_test_n)
-        # print(output_function_test_pred_n.shape)
-        # print(input_function_test_n[0,:,1])
-        plt.figure()
-        plt.grid(True, which="both", ls=":")
-        plt.plot(
-            input_function_test_n[0, :, 1].detach(),
-            output_function_test_n[0].detach(),
-            label="True Solution", c="C0", lw=2)
-        plt.scatter(
-            input_function_test_n[0, :, 1].detach(),
-            output_function_test_pred_n[0].detach(),
-            label="Approximate Solution", s=8, c="C1")
-        plt.xlabel("x")
-        plt.ylabel("u(x, 1)")
+            ax = fig.add_subplot(2, 3, index*3 + 1)
+            ax.imshow(x)
+            ax.set_title(f'Input x {resolution}')
+            plt.xticks([], [])
+            plt.yticks([], [])
 
-        err = self.error(output_function_test_n, output_function_test_pred_n)
-        print("Relative L2 error: ", err.item())
-        plt.legend()
+            ax = fig.add_subplot(2, 3, index*3 + 2)
+            ax.imshow(y)
+            ax.set_title('Ground-truth y')
+            plt.xticks([], [])
+            plt.yticks([], [])
+
+            ax = fig.add_subplot(2, 3, index*3 + 3)
+            ax.imshow(out)
+            ax.set_title('Model prediction')
+            plt.xticks([], [])
+            plt.yticks([], [])
+
+        fig.suptitle('Inputs, ground-truth output and prediction.', y=0.98)
+        plt.tight_layout()
+        fig.show()
