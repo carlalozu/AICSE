36a859da7dcb947bb58a4355732e1981d8de9d73
Carla Lopez <calopez@student.ethz.ch>
2024-05-20 01:43:05 +0200

    combine sfno into one file

--- project02/task2/sfno/fno02d.py
+++ project02/task2/sfno/fno02d.py
@@ -1,10 +1,9 @@
 """Fourier Neural Operator for parametrized PDEs."""
 from torch import nn
-import torch
 import torch.nn.functional as F
-import numpy as np
 
 from spectral_conv import SpectralConv2d
+from spherical_conv import SphericalConv2d
 
 
 class FNO2d(nn.Module):
@@ -29,8 +28,7 @@ class FNO2d(nn.Module):
 
         self.padding = 9  # pad the domain if input is non-periodic
         # input channel is 3: (a(x, y), x, y)
-        grid_dim = 2
-        self.fc0 = nn.Linear(3+grid_dim, self.width)
+        self.fc0 = nn.Linear(3, self.width)
 
         self.conv0 = SpectralConv2d(
             self.width, self.width, self.modes1, self.modes2)
@@ -49,8 +47,6 @@ class FNO2d(nn.Module):
         self.fc2 = nn.Linear(128, 3)
 
     def forward(self, x):
-        grid = self.get_grid(x.shape, x.device)
-        x = torch.cat((x, grid), dim=-1)
         x = self.fc0(x)
         x = x.permute(0, 3, 1, 2)
         x = F.pad(x, [0, self.padding, 0, self.padding])
@@ -81,14 +77,27 @@ class FNO2d(nn.Module):
         x = self.fc2(x)
         return x
 
-    def get_grid(self, shape, device):
-        batchsize, size_x, size_y, _ = shape
-        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
-        gridx = gridx.reshape(1, size_x, 1, 1).repeat(
-            [batchsize, 1, size_y, 1])
+class SFNO2d(FNO2d):
+    """The overall network. It contains 4 layers of the Spherical harmonics layer.
+    1. Lift the input to the desire channel dimension by self.fc0 .
+    2. 4 layers of the integral operators u' = (W + K)(u).
+        W defined by self.w; K defined by self.conv .
+    3. Project from the channel space to the output space by self.fc1 and self.fc2 .
 
-        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
-        gridy = gridy.reshape(1, 1, size_y, 1).repeat(
-            [batchsize, size_x, 1, 1])
+    input: the solution of the coefficient function and locations (a(x, y), x, y)
+    input shape: (batchsize, x=s, y=s, c=3)
+    output: the solution 
+    output shape: (batchsize, x=s, y=s, c=1)
+    """
 
-        return torch.cat((gridx, gridy), dim=-1).to(device)
+    def __init__(self, modes1, modes2,  width):
+        super().__init__(modes1, modes2,  width)
+
+        self.conv0 = SphericalConv2d(
+            self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SphericalConv2d(
+            self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SphericalConv2d(
+            self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SphericalConv2d(
+            self.width, self.width, self.modes1, self.modes2)
