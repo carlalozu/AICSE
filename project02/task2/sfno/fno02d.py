aabbfbb1306a30661ae281876ca4f60ab1fe960f
Carla Lopez <calopez@student.ethz.ch>
2024-05-20 00:20:50 +0200

    add padding, grid and conv2

--- project02/task2/sfno/fno02d.py
+++ project02/task2/sfno/fno02d.py
@@ -1,6 +1,8 @@
 """Fourier Neural Operator for parametrized PDEs."""
 from torch import nn
+import torch
 import torch.nn.functional as F
+import numpy as np
 
 from spectral_conv import SpectralConv2d
 
@@ -24,8 +26,11 @@ class FNO2d(nn.Module):
         self.modes1 = modes1
         self.modes2 = modes2
         self.width = width
+
+        self.padding = 9  # pad the domain if input is non-periodic
         # input channel is 3: (a(x, y), x, y)
-        self.fc0 = nn.Linear(3, self.width)
+        grid_dim = 2
+        self.fc0 = nn.Linear(3+grid_dim, self.width)
 
         self.conv0 = SpectralConv2d(
             self.width, self.width, self.modes1, self.modes2)
@@ -35,46 +40,55 @@ class FNO2d(nn.Module):
             self.width, self.width, self.modes1, self.modes2)
         self.conv3 = SpectralConv2d(
             self.width, self.width, self.modes1, self.modes2)
-        self.w0 = nn.Conv1d(self.width, self.width, 1)
-        self.w1 = nn.Conv1d(self.width, self.width, 1)
-        self.w2 = nn.Conv1d(self.width, self.width, 1)
-        self.w3 = nn.Conv1d(self.width, self.width, 1)
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.w1 = nn.Conv2d(self.width, self.width, 1)
+        self.w2 = nn.Conv2d(self.width, self.width, 1)
+        self.w3 = nn.Conv2d(self.width, self.width, 1)
 
         self.fc1 = nn.Linear(self.width, 128)
         self.fc2 = nn.Linear(128, 3)
 
     def forward(self, x):
-        batchsize = x.shape[0]
-        size_x, size_y = x.shape[1], x.shape[2]
-
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)
         x = self.fc0(x)
         x = x.permute(0, 3, 1, 2)
+        x = F.pad(x, [0, self.padding, 0, self.padding])
 
         x1 = self.conv0(x)
-        x2 = self.w0(x.view(batchsize, self.width, -1)
-                     ).view(batchsize, self.width, size_x, size_y)
+        x2 = self.w0(x)
         x = x1 + x2
-        x = F.relu(x)
+        x = F.gelu(x)
 
         x1 = self.conv1(x)
-        x2 = self.w1(x.view(batchsize, self.width, -1)
-                     ).view(batchsize, self.width, size_x, size_y)
+        x2 = self.w1(x)
         x = x1 + x2
-        x = F.relu(x)
+        x = F.gelu(x)
 
         x1 = self.conv2(x)
-        x2 = self.w2(x.view(batchsize, self.width, -1)
-                     ).view(batchsize, self.width, size_x, size_y)
+        x2 = self.w2(x)
         x = x1 + x2
-        x = F.relu(x)
+        x = F.gelu(x)
 
         x1 = self.conv3(x)
-        x2 = self.w3(x.view(batchsize, self.width, -1)
-                     ).view(batchsize, self.width, size_x, size_y)
+        x2 = self.w3(x)
         x = x1 + x2
 
+        x = x[..., :-self.padding, :-self.padding]
         x = x.permute(0, 2, 3, 1)
         x = self.fc1(x)
-        x = F.relu(x)
+        x = F.gelu(x)
         x = self.fc2(x)
         return x
+
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y, _ = shape
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat(
+            [batchsize, 1, size_y, 1])
+
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat(
+            [batchsize, size_x, 1, 1])
+
+        return torch.cat((gridx, gridy), dim=-1).to(device)
