20fb3d6741b1b93f41b6fe616b25f4ac206ba22a
Carla Lopez <calopez@student.ethz.ch>
2024-06-24 06:26:47 +0200

    wrap trainer into a class

--- project03/Task2/trainer.py
+++ project03/Task2/trainer.py
@@ -2,48 +2,63 @@ from neural_net import NNAnsatz
 import torch
 
 
-def loss_fn(y_pred, y_true):
-    """MSE loss function"""
-    diff = torch.mean((y_pred - y_true)**2)*100
-    # try to minimize the number of non-zero entries
-    non_zero = torch.mean(torch.where(y_pred > 0.01, torch.ones_like(y_pred), torch.zeros_like(y_pred)))
+class Trainer():
+    """Train the neural net to find the weights for the differential equation."""
 
-    return diff + 0.1*non_zero
+    def __init__(self, library, ground_truth) -> None:
+        self.library = library
+        self.dim = len(library)
+        self.model = NNAnsatz(input_dimension=self.dim,
+                              output_dimension=self.dim,
+                              n_hidden_layers=2,
+                              hidden_size=10)
+        self.ground_truth = ground_truth
 
-def train(library, y_true, epochs=1000):
-    """Find weights for the differential equations"""
-    dim = len(library)
+    @staticmethod
+    def _loss_fn(y_pred, y_true):
+        """MSE loss function"""
+        diff = torch.mean((y_pred - y_true)**2)*100
+        # try to minimize the number of non-zero entries
+        non_zero = torch.mean(torch.where(
+            y_pred > 0.01, torch.ones_like(y_pred), torch.zeros_like(y_pred)))
 
-    components_ = torch.stack(library, dim=1).squeeze()
+        return diff + 0.1*non_zero
 
-    model = NNAnsatz(input_dimension=dim,
-                     output_dimension=dim,
-                     n_hidden_layers=2,
-                     hidden_size=10)
+    def train(self, epochs=1000, lr=0.01, step_size=1000, gamma=0.5):
+        """Find weights for the differential equations"""
+        components_ = torch.stack(self.library, dim=1).squeeze()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.5)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        if step_size:
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer, step_size=step_size, gamma=gamma)
 
-    # Initialize weights
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        weights = torch.ones(dim, requires_grad=True)        
-        weights = model(weights)
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            weights = torch.ones(self.dim, requires_grad=True)
+            weights = self.model(weights)
 
-        y_pred = torch.sum(weights.view(dim, 1, 1) * components_, dim=0)
-        loss = loss_fn(y_pred, y_true.squeeze())
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
+            y_pred = torch.sum(weights.view(self.dim, 1, 1)
+                               * components_, dim=0)
+            loss = self._loss_fn(y_pred, self.ground_truth.squeeze())
+            loss.backward()
+            optimizer.step()
+            if step_size:
+                scheduler.step()
 
-        if epoch % 1000 == 0:
-            print(f'Epoch {epoch}, loss {loss.item()}')
+            if epoch % 1000 == 0:
+                print(f'Epoch {epoch}, loss {loss.item()}')
 
-    return weights
+        return weights
 
-def test(library, weights):
-    """Test the neural net"""
-    dim = len(library)
-    library_ = torch.stack(library, dim=1).squeeze()
-    y_pred = torch.sum(weights.view(dim, 1, 1) * library_, dim=0)
-    return y_pred.reshape(1, *y_pred.shape)
+    def test(self, weights):
+        """Test the neural net"""
+        library_ = torch.stack(self.library, dim=1).squeeze()
+        y_pred = torch.sum(weights.view(self.dim, 1, 1) * library_, dim=0)
+        return y_pred.reshape(1, *y_pred.shape)
+
+    def print_eq(self, weights):
+        """Print the differential equation"""
+        used = [f'{weight:.2e}*{term}' for term,
+                weight in zip(self.library['terms'], weights) if weight != 0]
+        print(' + '.join(used), '=d/dt')
